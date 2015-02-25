17775d6c373252dc12c2aaa36fc8b639133dfb80
森崎 真悟 <morisaki.singo@butterfly-corp.jp>
2015-02-25 14:00:15 +0900

    c

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := jnitest
 LOCAL_SRC_FILES := jnitest.c
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
---
+++ jni/main.c
@@ -0,0 +1,7 @@
+#include <jni.h>
+#include <errno.h>
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+#include <math.h>
+#include <android/sensor.h>
+#include <android_naiteve_app_glue.h>
